3487ff78c5aae802b56d222e644437a5ec895c7e
Caleb Hamilton <cjlhamilton@gmail.com>
2017-08-09 12:53:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# spotify-lyrics
+
+A Python script for displaying the lyrics of Spotify's current playing song in the Linux terminal
+
+![Screenshot](./screenshots/1.png "Screenshot")
+
+## How to use
+2. Place `spotify-lyrics.py` in a directory of your choice
+1. Run `python3 spotify-lyrics.py` from the same directory
+
+***
+
+[https://github.com/cjlh/spotify-lyrics](https://github.com/cjlh/spotify-lyrics)
---
+++ spotify-lyrics.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# spotify-lyrics.py (https://github.com/cjlh/spotify-lyrics)
+# Author: Caleb Hamilton
+# Website: http://calebh.com
+
+import os
+import time
+import dbus
+import requests
+old_song_info = None
+
+def get_song_info():
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotify_metadata = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties").Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    return [str(spotify_metadata['xesam:artist'][0].title()), str(spotify_metadata['xesam:title'])]
+
+def center_string(s):
+    terminal_cols = os.popen('tput cols').read()
+    return str("{:^" + str(int(terminal_cols)+10) + "}").format(s)
+
+def print_lyrics(artist, title):
+    print(center_string("\n---\n\n"))
+    print(center_string("\033[4m" + artist + ": " + title + "\033[0m\n"))
+    pageurl = "https://makeitpersonal.co/lyrics?artist=" + artist + "&title=" + title
+    lyrics = requests.get(pageurl).text.strip()
+    #print("\033[0;37m" + lyrics + "\033[0m")
+    print(lyrics)
+
+while True:
+    song_info = get_song_info()
+    if song_info != old_song_info:
+        old_song_info = song_info
+        os.system('clear')
+        print_lyrics(song_info[0], song_info[1])
+    time.sleep(1)
