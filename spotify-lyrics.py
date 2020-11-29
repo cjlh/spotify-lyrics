e8e0a18dcc8bf241a009bbfe66b9f24d37236a2d
Caleb Hamilton <cjlhamilton@gmail.com>
2020-11-29 16:48:54 +0000

    Show error message if Spotify is not running; minor refactoring

--- spotify-lyrics.py
+++ spotify-lyrics.py
@@ -13,21 +13,21 @@ Usage:
     $ python spotify-lyrics.py
 """
 import argparse
+import dbus
 import os
 import re
+import requests
+import sys
 import time
 import unicodedata
 
-import dbus
-import requests
 from bs4 import BeautifulSoup
 
 
 def get_song_info():
     session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object(
-        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2"
-    )
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                         "/org/mpris/MediaPlayer2")
 
     spotify_metadata = dbus.Interface(
         spotify_bus, "org.freedesktop.DBus.Properties"
@@ -59,22 +59,24 @@ def letras_mus_provider(artist, title):
         return ""
 
     lyric_box = str(lyric_box)
-    lyrics = lyric_box.replace('<div class="cnt-letra p402_premium">', "")
-    lyrics = lyrics.replace("</div>", "")
-    lyrics = lyrics.replace("<br/>", "\n")
-    lyrics = lyrics.replace("</br>", "\n")
-    lyrics = lyrics.replace("<br>", "\n")
-    lyrics = lyrics.replace("</br>", "\n")
-    lyrics = lyrics.replace("</p><p>", "\n\n")
-    lyrics = lyrics.replace("</p>", "")
-    return lyrics.replace("<p>", "").strip()
+    
+    return lyric_box.replace('<div class="cnt-letra p402_premium">', "") \
+                    .replace("</div>", "") \
+                    .replace("<br/>", "\n") \
+                    .replace("</br>", "\n") \
+                    .replace("<br>", "\n") \
+                    .replace("</br>", "\n") \
+                    .replace("</p><p>", "\n\n") \
+                    .replace("</p>", "") \
+                    .replace("<p>", "").strip()
 
 
 def make_it_personal_provider(artist, title):
     pageurl = f"https://makeitpersonal.co/lyrics?artist={artist}&title={title}"
     lyrics = requests.get(pageurl).text.strip()
 
-    if lyrics == "Sorry, We don't have lyrics for this song yet.":
+    if (lyrics == "Sorry, We don't have lyrics for this song yet. Add them to "
+                  "https://lyrics.wikia.com"):
         return ""
 
     return lyrics
@@ -93,10 +95,10 @@ def fandom_provider(artist, title):
     if not lyric_box:
         return ""
 
-    lyrics = str(lyric_box).replace("<br/>", "\n")
-    lyrics = lyrics.replace('<div class="lyricbox">', "")
-    lyrics = lyrics.replace('<div class="lyricsbreak">', "")
-    return lyrics.replace("</div>", "")
+    return str(lyric_box).replace("<br/>", "\n") \
+                         .replace('<div class="lyricbox">', "") \
+                         .replace('<div class="lyricsbreak">', "") \
+                         .replace("</div>", "")
 
 
 def format_request_param(request_param):
@@ -106,7 +108,6 @@ def format_request_param(request_param):
 
 
 def print_lyrics(artist, title, lyrics_provider_name):
-    print(center_string("\n---\n\n"))
     print(center_string("\033[4m" + artist + ": " + title + "\033[0m\n"))
 
     artist = format_request_param(artist)
@@ -114,18 +115,29 @@ def print_lyrics(artist, title, lyrics_provider_name):
     lyrics = ""
 
     if lyrics_provider_name:
-        lyrics_provider = providers[lyrics_provider_name]
+        lyrics_provider = PROVIDERS[lyrics_provider_name]
         lyrics = lyrics_provider(artist, title)
     else:
-        for lyrics_provider in providers.values():
+        for lyrics_provider in PROVIDERS.values():
             lyrics = lyrics_provider(artist, title)
             if lyrics:
                 break
 
-    print(lyrics or "Lyrics not found")
+    print(lyrics or "Lyrics could not be found for this song.")
 
 
-providers = {
+def main():
+    old_song_info = None
+    while True:
+        song_info = get_song_info()
+        if song_info != old_song_info:
+            old_song_info = song_info
+            os.system("clear")
+            print_lyrics(song_info[0], song_info[1], args.lyrics_provider)
+        time.sleep(1)
+
+
+PROVIDERS = {
     "make_it_personal": make_it_personal_provider,
     "fandom": fandom_provider,
     "letras_br": letras_mus_provider,
@@ -135,15 +147,14 @@ providers = {
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--lyrics-provider", help="Lyrics Provider", choices=providers.keys(),
+        "--lyrics-provider", help="Lyrics Provider", choices=PROVIDERS.keys(),
     )
     args = parser.parse_args()
 
-    old_song_info = None
-    while True:
-        song_info = get_song_info()
-        if song_info != old_song_info:
-            old_song_info = song_info
-            os.system("clear")
-            print_lyrics(song_info[0], song_info[1], args.lyrics_provider)
-        time.sleep(1)
+    try:
+        main()
+    except dbus.exceptions.DBusException:
+        print("Spotify does not appear to be running! Please start Spotify and try "
+              "again.", file=sys.stderr)
+        exit(1)
+
